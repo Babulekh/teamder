c9d4e62467187012b28200795a8f2dd0bd8d83f2
Babulekh <vanca624@gmail.com>
2023-04-29 23:46:43 +0300

    Deleted frontend from docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,6 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   server: {
     port: 8081,
-    host: "0.0.0.0",
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-    }
   },
   resolve: {
     alias: {
